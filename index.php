7040a0711393931e5e042a4cd0b54f375ff81761
Othman <othman.douz07@gmail.com>
2023-06-08 11:28:27 +0200

    test

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['username'])) {
 
     <?php
     // Connexion à la base de données
-    $conn = new PDO('mysql:host=localhost;dbname=votre_base_de_donnees', 'nom_utilisateur', 'mot_de_passe');
+    $conn = new PDO('mysql:host=localhost;dbname=MySQL', 'root', 'AZERTYUIOP');
 
     // Récupération des personnages de l'utilisateur connecté
     $username = $_SESSION['username'];
